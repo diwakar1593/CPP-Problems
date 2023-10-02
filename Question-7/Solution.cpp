c88d7426b841fa3115d851a6e4a69fcc11015654
Shikha Srivastava <92089976+Shikha785@users.noreply.github.com>
2023-10-02 17:04:49 +0530

    Created Solution.cpp
    Added solution of problem statement

---
+++ Question-7/Solution.cpp
@@ -0,0 +1,14 @@
+//Ugly Number
+class Solution {
+public:
+    bool isUgly(int n) {
+        while(n>1){
+            if(n%2==0) n = n/2;
+            else if(n%3==0) n = n/3;
+            else if(n%5==0) n = n/5;
+            else return 0;
+        }
+        if(n==1) return true;
+        return false;
+    }
+};
